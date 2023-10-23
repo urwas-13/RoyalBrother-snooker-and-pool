085221b6a2ce7dadaaeaa54a1812403e165fd4d4
Urwashi Dhande <136886288+urwas-13@users.noreply.github.com>
2023-10-23 14:39:39 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+
+var fullImgBox = document.querySelector("#fullImgBox");
+var fullImg = document.querySelector("#fullImg");
+
+function openFullImg(pic){
+    fullImgBox.style.display = "flex";
+    fullImg.src = pic;
+}
+
+ function closeFullImg(){
+    fullImgBox.style.display = "none";
+ }
+
+
+
+
+
+
+
+
+
+
